8fdf33d8b53fbb2efea1f13df7bdc98a76184119
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-02-19 15:17:24 -0800

    before mix phx.gen.auth

--- phoenix/base1/TODO.md
+++ phoenix/base1/TODO.md
@@ -1,6 +1,7 @@
 - [x] set up heex and emmet extensions
 - [x] try liveview
 - [x] create users database table
+- [ ] try mix phx.gen.auth
 - [ ] set up github Oauth
 - [ ] set up create user with invite code
 - [ ] set up sessions/auth for protected routes
--- phoenix/base1/priv/repo/migrations/20230219142956_create_users_table.exs
+++ phoenix/base1/priv/repo/migrations/20230219142956_create_users_table.exs
@@ -2,33 +2,33 @@ defmodule App.Repo.Migrations.CreateUsersTable do
   use Ecto.Migration
 
   def change do
-    execute("CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;")
+    # execute("CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;")
 
-    create table(:users) do
-      add :email, :citext, null: false
-      add :provider, :string
-      add :token, :string
+    # create table(:users) do
+    #   add :email, :citext, null: false
+    #   add :provider, :string
+    #   add :token, :string
 
-      timestamps()
-    end
+    #   timestamps()
+    # end
 
-    create unique_index(:users, [:email])
+    # create unique_index(:users, [:email])
 
-    create table(:profiles) do
-      add :username, :citext, null: false
-      add :user_id, references(:users)
+    # create table(:profiles) do
+    #   add :username, :citext, null: false
+    #   add :user_id, references(:users)
 
-      timestamps()
-    end
+    #   timestamps()
+    # end
 
-    create unique_index(:profiles, [:user_id])
+    # create unique_index(:profiles, [:user_id])
 
-    create table(:invite_codes) do
-      add :code, :string, null: false
+    # create table(:invite_codes) do
+    #   add :code, :string, null: false
 
-      timestamps()
-    end
+    #   timestamps()
+    # end
 
-    create unique_index(:invite_codes, [:code])
+    # create unique_index(:invite_codes, [:code])
   end
 end
